057e792c0d4e448907204d7ff41de7c6e6c8e3fc
Rich Finn <12137272+RichFinn-WTW@users.noreply.github.com>
2025-02-07 12:05:55 -0700

    configure main

--- src/org/apps/host/src/main.ts
+++ src/org/apps/host/src/main.ts
@@ -1,1 +1,15 @@
-import('./bootstrap').catch((err) => console.error(err));
+import { setRemoteDefinitions } from '@nx/angular/mf';
+
+//create cache buster for module federation manifests. We have a different mechanism we use for this in our application, but for the sake of this example, using a random guid is sufficient
+const id = crypto.randomUUID();
+
+import('./mfe-manifest')
+    .then((m) => m.getManifest(id))
+    .then((manifest) => {
+        setRemoteDefinitions(manifest);
+        return manifest;
+    })
+    .then((manifest) => import('./bootstrap').then((bs) => bs.createOrgApplication(manifest)))
+
+
+// import('./bootstrap').catch((err) => console.error(err));
