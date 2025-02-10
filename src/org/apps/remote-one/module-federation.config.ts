3a4c3029c91b1cbee5c3b24adebe7dff2d8b2105
Rich Finn <12137272+RichFinn-WTW@users.noreply.github.com>
2025-02-10 08:03:36 -0700

    add components module to remote-one mfe config

--- src/org/apps/remote-one/module-federation.config.ts
+++ src/org/apps/remote-one/module-federation.config.ts
@@ -4,6 +4,7 @@ const config: ModuleFederationConfig = {
   name: 'remote-one',
   exposes: {
     './Routes': 'apps/remote-one/src/app/remote-entry/entry.routes.ts',
+    './Components': 'apps/remote-one/src/app/remote-entry/component-map.ts',
   },
 };
 
