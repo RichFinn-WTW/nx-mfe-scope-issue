0af5a63a465f998c09432bc6fcf1618b0423f6f7
Rich Finn <12137272+RichFinn-WTW@users.noreply.github.com>
2025-02-10 08:03:10 -0700

    add component map to remote

---
+++ src/org/apps/remote-one/src/app/remote-entry/component-map.ts
@@ -0,0 +1,5 @@
+import { SimpleComponent } from "../SimpleComponent/Simple.component";
+
+export const entryComponentsMap = {
+    "SimpleComponent": SimpleComponent //() => import('../SimpleComponent/Simple.component').then(m => m.SimpleComponent),
+}
